96dc8a9f6495bf1dca4f6f99d08f9d360af55fda
Murat KIRDAL <muratkirdal0101@gmail.com>
2025-06-03 21:30:06 +0300

    First Commit

---
+++ Assets/Scripts/Runtime/Utilities/ConstantsUtilities.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+namespace Runtime.Utilities
+{
+    public static class ConstantsUtilities
+    {
+        
+#region Vectors
+        
+        public static readonly Vector2 Zero2 = Vector2.zero;
+        
+        public static readonly Vector3 Zero3 = Vector3.zero;
+        
+#endregion
+
+#region Layer
+
+        public static readonly int AllLayerMask = ~0;
+
+#endregion
+
+#region LayerMask
+        
+
+
+#endregion
+        
+#region Tags
+
+
+
+#endregion
+            
+#region Animation Hash
+
+
+
+#endregion
+
+    }
+}
